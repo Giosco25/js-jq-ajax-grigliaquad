67c706ca3f08d56bb121b9d82ef68a6d9b39de47
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-05-19 15:52:41 +0200

    sistemazione API

--- main.js
+++ main.js
@@ -3,25 +3,25 @@
 $('.quadratini').click(function(){
 console.log('click');
 // Ad ogni click su un quadratino, parte una richiesta ajax per recuperare un numero random tra 1 e 9.
+var quadratino = $(this);
  $.ajax({
      'url': 'https://flynn.boolean.careers/exercises/api/random/int',
      'method': 'GET',
      'success':function(data){
-         console.log(data);
+         // console.log(data);
          var numero_random = data.response;
          console.log(numero_random);
          // Se il numero restituito dall'api è <= 5, il quadrato su cui l'utente ha cliccato diventa giallo;
          if (numero_random <= 5) {
-             $(this).addClass('giallo')
-             console.log(this);
+             quadratino.addClass('giallo')
+             // console.log(this);
              // se invece il numero restituito dall'api è > 5, il quadrato su cui l'utente ha cliccato diventa verde.
          } else {
-             $(this).addClass('verde')
-             console.log(this);
+            quadratino.addClass('verde')
+             // console.log(this);
          }
-          // $(this).append(numero_random);
+         // In entrambi i casi, andiamo ad inserire nel quadrato il numero restituito dall'api.
+          quadratino.append(numero_random);
      }
  })
 });
-
-// In entrambi i casi, andiamo ad inserire nel quadrato il numero restituito dall'api.
